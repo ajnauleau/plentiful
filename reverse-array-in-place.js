f12a70227fae97a2cb82abe2cb6e64c2df6fc287
Antoine Nauleau <ajnauleau@gmail.com>
2018-07-30 18:42:56 +0200

    Algorithm checks out.

---
+++ reverse-array-in-place.js
@@ -0,0 +1,17 @@
+const reverse = (array) => {
+
+  length = array.length;
+  middle = Math.floor(length/2);
+  hold = null;
+
+  for(let i=0; i<middle; i++) {
+    hold = array[i];
+    array[i] = array[length-(i+1)];
+    array[length-(i+1)] = hold;
+  }
+
+  return array;
+
+}
+
+reverse([0, 1, 2, 3, 4]);
