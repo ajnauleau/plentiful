989d619d8480f648745c9a4559fa7fab15a54025
Antoine Nauleau <ajnauleau@gmail.com>
2018-09-09 23:58:40 -0700

    Finish binary-search, needs testing

--- search/binary-search.js
+++ search/binary-search.js
@@ -1,25 +1,25 @@
 const binarySearch = (array, num, start, end) => {
-    if (start == end) {
-        if (array[start] > num) {
-            return start;
-        } else {
-            return start + 1;
-        }
-    }
-
-    if (start > end) {
-        return start;
-    }
-
-    let mid = round((start + end) / 2);
-
-    if (array[mid] < num) {
-        return binarySearch(array, num, mid + 1, end);
-    } else if (array[mid] > num) {
-        return binarySearch(array, item, start, mid - 1);
+  if (start == end) {
+    if (array[start] > num) {
+      return start;
     } else {
-        return mid;
+      return start + 1;
     }
+  }
+
+  if (start > end) {
+    return start;
+  }
+
+  let mid = Math.round((start + end) / 2);
+
+  if (array[mid] < num) {
+    return binarySearch(array, num, mid + 1, end);
+  } else if (array[mid] > num) {
+    return binarySearch(array, num, start, mid - 1);
+  } else {
+    return mid;
+  }
 };
 
-export default binarySearch;
+console.log(binarySearch([1, 3, 7, 5, 6, 9, 1, 3, 4], 1, 8, 9));
